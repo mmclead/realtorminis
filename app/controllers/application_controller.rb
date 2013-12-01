c0a410abd6a9a87db1e556751e99ad9fbf5e6f3f
Mason McLead <mason@lapshare.com>
2013-11-30 16:09:56 -0800

    route users to their user page at login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def after_sign_in_path_for(resource_or_scope)
+    current_user
+  end
+
   rescue_from CanCan::AccessDenied do |exception|
     url = user_signed_in? ? current_user : root_url
     redirect_to url, :alert => exception.message.present? ? exception.message : "You don't have permission for that."
