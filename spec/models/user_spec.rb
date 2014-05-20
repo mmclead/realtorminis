5a5b685a3b31764864e96e90f353e6f233e9264d
Mason McLead <mason@lapshare.com>
2014-05-20 01:17:29 -0700

    ading specs

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+describe User do
+  let(:user) {FactoryGirl.create(:user)}
+
+  describe 'associations' do
+    it { should have_many :listings }
+    it { should have_many :sites }
+  end
+
+
+  describe 'profile_attribute_list' do
+    it 'calls the same method on its profile' do
+      expect(user.profile).to receive(:profile_attribute_list)
+      
+      user.profile_attribute_list
+    end
+  end
+
+  describe 'profile_hash' do
+
+    it 'calls the same method on its profile' do
+      expect(user.profile).to receive(:profile_hash)
+
+      user.profile_hash
+    end
+  end
+
+end
