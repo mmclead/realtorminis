2b3753ec2ee3ac5493d3730bea4bed18db40888f
Mason McLead <mason@lapshare.com>
2015-06-20 23:11:10 -0700

    updating phone number in the regristration contact info

--- config/initializers/domain_registration_settings.rb
+++ config/initializers/domain_registration_settings.rb
@@ -3,13 +3,13 @@ contact_info = {
   last_name: "McLead",
   contact_type: "COMPANY",
   organization_name: "RealtorMinis",
-  address_line_1: "2403 Rockefeller Lane ",
+  address_line_1: "2403 Rockefeller Lane",
   address_line_2: "Unit A",
   city: "Redondo Beach",
   state: "CA",
   country_code: "US",
   zip_code: "90278",
-  phone_number: "+13109621144",
+  phone_number: "+1.3109621144",
   email: "mason@realtorminis.com"
 }
 
